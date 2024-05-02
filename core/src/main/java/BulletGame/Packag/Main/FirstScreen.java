ae4558dec07866176f1b02426fce77a0c9fae208
NateP <bulletmagnet.123@gmail.com>
2024-05-01 20:26:14 -0700

    made a pointer dot that says where your pointing then draws a red circle where the cursor/finger is touching and draws a simple texture of the knight that im trying to implement

--- core/src/main/java/BulletGame/Packag/Main/FirstScreen.java
+++ core/src/main/java/BulletGame/Packag/Main/FirstScreen.java
@@ -1,30 +1,59 @@
 package BulletGame.Packag.Main;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.math.Vector3;
 
 /** First screen of the application. Displayed after the application is created. */
 public class FirstScreen implements Screen {
     SpriteBatch batch;
-
-    Texture texture;
+    Texture porn;
+    Texture Knight;
+    private OrthographicCamera camera;
+    ShapeRenderer shapeRenderer;
+    Vector3 pos;
 
     @Override
     public void show() {
         // Prepare your screen here.
         batch = new SpriteBatch();
-        texture = new Texture("porn.jpg");
+
+        porn = new Texture("com/porn/porn.jpg");
+        Knight = new Texture("com/sprites/knight.png");
+        shapeRenderer = new ShapeRenderer();
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        pos = new Vector3( Gdx.graphics.getWidth() / 2, Gdx.graphics.getHeight() / 2, 0);
+
     }
 
     @Override
     public void render(float delta) {
         // Draw your screen here. "delta" is the time since last render in seconds.
         batch.begin();
-
-        batch.draw(texture, 0, 0);
-
+        Gdx.gl.glClearColor(0, 0, 0, 0);
+        Gdx.gl.glClear(Gdx.gl20.GL_COLOR_BUFFER_BIT);
+        camera.update();
+        batch.draw(Knight, 50, 50);
+        //batch.draw(porn, 0, 0);
+        if (Gdx.input.isTouched()) {
+            float x = Gdx.input.getX();
+            float y = Gdx.graphics.getHeight() - Gdx.input.getY();
+            System.out.println("x: " + x + " y: " + y);
+            pos.set(x, y, 0);
+        }
         batch.end();
+        shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
+        shapeRenderer.setColor(Color.RED);
+        shapeRenderer.circle(pos.x, pos.y, 20);
+        shapeRenderer.end();
+
+
     }
 
     @Override
