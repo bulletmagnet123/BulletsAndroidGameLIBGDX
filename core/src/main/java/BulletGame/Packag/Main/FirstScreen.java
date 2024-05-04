c113e1f6ab27e4efaebec06c45fdecc472e0a3c8
NateP <bulletmagnet.123@gmail.com>
2024-05-03 20:23:27 -0700

    got the idle animation working and cleaned up some code and stuff. next job is to. get the basic controls going

--- core/src/main/java/BulletGame/Packag/Main/FirstScreen.java
+++ core/src/main/java/BulletGame/Packag/Main/FirstScreen.java
@@ -5,7 +5,9 @@ import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.math.Vector3;
 
@@ -13,10 +15,16 @@ import com.badlogic.gdx.math.Vector3;
 public class FirstScreen implements Screen {
     SpriteBatch batch;
     Texture porn;
-    Texture Knight;
+    Texture KnightSheet;
     private OrthographicCamera camera;
     ShapeRenderer shapeRenderer;
     Vector3 pos;
+    private static final int FRAME_COLS = 4, FRAME_ROWS = 1;
+
+    Animation<TextureRegion> IdleAnimation;
+    Texture walkSheet;
+
+    float stateTime;
 
     @Override
     public void show() {
@@ -24,11 +32,28 @@ public class FirstScreen implements Screen {
         batch = new SpriteBatch();
 
         porn = new Texture("com/porn/porn.jpg");
-        Knight = new Texture("com/sprites/knight.png");
+        KnightSheet = new Texture("com/sprites/knight.png");
         shapeRenderer = new ShapeRenderer();
         camera = new OrthographicCamera();
         camera.setToOrtho(false, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
         pos = new Vector3( Gdx.graphics.getWidth() / 2, Gdx.graphics.getHeight() / 2, 0);
+
+        //COPIED CODE PLEASE REVISE
+
+        // Load the sprite sheet as a Texture
+
+
+        // Use the split utility method to create a 2D array of TextureRegions. This is
+        // possible because this sprite sheet contains frames of equal size and they are
+        // all aligned.
+        TextureRegion[][] tmp = TextureRegion.split(KnightSheet, 32, 32);
+        TextureRegion[] KnightIdleFrames = new TextureRegion[FRAME_COLS];
+        int index = 0;
+        for (int j = 0; j < FRAME_COLS; j++) {
+            KnightIdleFrames[index++] = tmp[0][j];
+        }
+        IdleAnimation = new Animation<TextureRegion>(0.25f, KnightIdleFrames);
+        stateTime = 0f;
 
     }
 
@@ -36,11 +61,15 @@ public class FirstScreen implements Screen {
     public void render(float delta) {
         // Draw your screen here. "delta" is the time since last render in seconds.
         batch.begin();
+        stateTime += Gdx.graphics.getDeltaTime();
+
         Gdx.gl.glClearColor(0, 0, 0, 0);
         Gdx.gl.glClear(Gdx.gl20.GL_COLOR_BUFFER_BIT);
         camera.update();
-        batch.draw(Knight, 50, 50);
+//        batch.draw(KnightSheet, 50, 50);
         //batch.draw(porn, 0, 0);
+        TextureRegion currentFrame = IdleAnimation.getKeyFrame(stateTime, true);
+        batch.draw(currentFrame, 50, 50); // Draw current frame at (50, 50)
         if (Gdx.input.isTouched()) {
             float x = Gdx.input.getX();
             float y = Gdx.graphics.getHeight() - Gdx.input.getY();
